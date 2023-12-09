919e8972456aa87c98a23210ed347db51624b032
premdeep909 <premdeep909@gmail.com>
2023-12-09 19:21:01 +0530

    Task - 3 Done By Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+ var headerTitle = document.getElementById('main-header');
+ headerTitle.style.borderBottom = 'solid 3px #000';
+var addItems = document.getElementsByClassName('title');
+addItems[0].style.fontWeight = "bold";
+addItems[0].style.color = "green";
+
