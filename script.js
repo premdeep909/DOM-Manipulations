72c0c12b3d07f0153ac979cbb6c25d0e8f5ac0c3
premdeep909 <premdeep909@gmail.com>
2023-12-10 11:01:48 +0530

    Task - 5 GetElementByTagName

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var addItems = document.getElementsByClassName('title');
 addItems[0].style.fontWeight = "bold";
 addItems[0].style.color = "green";
 
-var item = document.getElementsByClassName('list-group-item');
-item[2].style.backgroundColor ='green';
-
+//var item = document.getElementsByClassName('list-group-item');
+//item[2].style.backgroundColor ='green';
+//for(var i = 0;i<item.length;i++){
+   // item[i].style.backgroundColor = 'red';
+//}
 var items = document.getElementsByTagName('li');
 for(var i = 0;i<items.length;i++){
    items[i].style.fontWeight ='bold';
-}
+   }
