d51a6984ddb0e0528457ab02728935fe83c18f61
premdeep909 <premdeep909@gmail.com>
2023-12-10 10:19:04 +0530

    Task - 4 Done By JavaScript

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var addItems = document.getElementsByClassName('title');
 addItems[0].style.fontWeight = "bold";
 addItems[0].style.color = "green";
 
+var item = document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor ='green';
+
+var items = document.getElementsByTagName('li');
+for(var i = 0;i<items.length;i++){
+   items[i].style.fontWeight ='bold';
+}
